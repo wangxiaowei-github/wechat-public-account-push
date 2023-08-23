a960e79555269a6d0cb45fe75a546018b67ea9fb
wangxiaowei-github <88219687+wangxiaowei-github@users.noreply.github.com>
2023-08-23 13:30:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '婉婉呐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oCmsK6lNOLGX4ajMsOZByfulmhKo',
+      id: 'oCmsK6miG1uro95pjJiKf48WZHGo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'qKMkgjIxJhgK9STX3NP7wWvnDoBNNfWw7kLdTEIAgRM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: 'admin',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oCmsK6lNOLGX4ajMsOZByfulmhKo',
+      id: 'oCmsK6miG1uro95pjJiKf48WZHGo',
     }
   ],
 
